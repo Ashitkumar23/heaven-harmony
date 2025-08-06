0e7d32e938a2d893dc3c742ee195590c766cfbc0
ashitkumar23 <ashit.23kumar@gmail.com>
2025-08-06 10:09:53 +0530

    corected name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Featured_products from "../components/Featured_products";
 import Hero_section from "../components/Hero_section";
 import New_arrival from "../components/New_arrival";
 import Special_offer from "../components/Special _offer";
-import Testimonials from "../components/testimonials";
+import Testimonials from "../components/Testimonials";
 
 export default function Home() {
   return (
